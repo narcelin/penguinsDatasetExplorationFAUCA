bcc68d88273372dbda188d22413cc6f8c00ef8d4
Nicolas Arcelin <narcelin@Nicolass-MacBook-Pro.local>
2024-09-15 10:19:37 -0400

    completed assignment

--- penguinsdatasetexploration.py
+++ penguinsdatasetexploration.py
@@ -17,18 +17,22 @@ import numpy as np
 
 # Reading dataset
 p_df = pd.read_csv('penguins.csv')
-p_df.head()
+print(p_df.head())
 
 """# Task 2: Handling Missing Data"""
 
 # Check for null data
-p_df.isnull().any()
+print("Start of NULL values search \n")
+print(p_df.isnull().any())
+print("\nEnd NULL values search \n")
 
 # Drop rows with any column having NA/null data
 np_df = p_df.dropna()
 
 # Check new dataframe for null values
-np_df.isnull().any()
+print("Start of NULL values search \n")
+print(np_df.isnull().any())
+print("\nEnd NULL values search \n")
 
 """# Task 3: Summary Statistics"""
 
@@ -41,7 +45,10 @@ print('\n 3)', np_df['sex'].value_counts())
 
 """# Task 3: Visualizing the Data"""
 
-sns.pairplot(np_df, hue='species')
+np_df_n = np_df.iloc[:, 2:6]
+print(np_df_n.head())
+
+sns.pairplot(np_df_n, hue='body_mass_g')
 
 # Create subplots
 fig, axes = plt.subplots(1, 3, figsize=(20, 5))
@@ -66,8 +73,36 @@ plt.show()
 
 """# Task 5: Covariance Matrix for Numeric Attributes"""
 
-# Computing covariance matrix for all numeric attribute
-numeric_df = np_df.select_dtypes(include=['number'])
-numeric_df
-# covariance_matrix = numeric_df.cov()
-# covariance_matrix
+# cov_matrix of num
+D = np_df_n.to_numpy()
+D_center = D - np.mean(D, axis=0)
+cov_matrix = np.cov(D_center, rowvar=False)
+
+print(cov_matrix)
+
+# The value of covariance represents the
+# strength in the relationship
+# between those two attributes.
+# For example Covariance between attribute 1 and 3 is 50.058
+# which represents a strong relationship while the relationship between
+# attribute 2 and 3 is -15.948 referencing a weak relationship between the two.
+
+np_df_a = np_df.iloc[:, [0, 1, 6]]
+print("\n\n", np_df_a)
+
+np_df_a_encoded = pd.get_dummies(np_df_a)
+print(np_df_a_encoded)
+
+cov_np_df_a_encoded = np_df_a_encoded.cov()
+print(cov_np_df_a_encoded)
+
+# Correlation Matrix
+
+corr_matrix = np_df_n.corr()
+print(corr_matrix)
+
+# Create a heatmap of the correlation matrix
+plt.figure(figsize=(8, 6))  # Set the figure size
+sns.heatmap(corr_matrix)
+plt.title('Correlation Matrix Heatmap')
+plt.show()
